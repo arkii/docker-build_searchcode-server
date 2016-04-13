a27b4d12e6558aba32654557ab1415cf73969647
arkii <sqy6@163.com>
2016-04-13 14:44:21 +0800

    1.0.1

--- tools/update_dockerfile.sh
+++ tools/update_dockerfile.sh
@@ -4,5 +4,5 @@ sed -i -e "/SERVER_VERSION/s/SERVER_VERSION.*$/SERVER_VERSION ${version}/" Docke
 git add . && \
 git commit -m "${version}" && \
 git tag -f "${version}" && \
-# git push origin --tags
-git push --tags --force
+git push origin --tags --force
+# git push --tags --force
