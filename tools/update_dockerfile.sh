a0530e94d0e83bfa62e2ba6d656bcd8b5e06d601
arkii <sqy6@163.com>
2016-04-13 16:33:42 +0800

    1.0.1

--- tools/update_dockerfile.sh
+++ tools/update_dockerfile.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 version=$(wget -q -O /dev/stdout https://searchcode.com/product/version/ | tr -d \") && \
 sed -i -e "/SERVER_VERSION/s/SERVER_VERSION.*$/SERVER_VERSION ${version}/" Dockerfile && \
-# git add . && \
 git commit -a -m "${version}" && \
-git push
 git tag -f "${version}" && \
-# git push origin --tags --force
-git push --tags --force
+git push origin :refs/tags/${version}
+git push
+git push --tags
